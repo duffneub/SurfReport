8181da8b539aaf640f020f0be75e40add74cb701
Duff Neubauer <d.neubauer@me.com>
2021-03-26 08:19:35 -0700

    Initial Commit

---
+++ SurfReport/SurfReportApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurfReportApp.swift
+//  SurfReport
+//
+//  Created by Duff Neubauer on 3/26/21.
+//
+
+import SwiftUI
+
+@main
+struct SurfReportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
