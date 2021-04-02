5205ed47464541182a7605d34cdae07d36c9f498
Duff Neubauer <d.neubauer@me.com>
2021-04-02 16:09:59 -0700

    Add Google Firebase

--- SurfReport/SurfReportApp.swift
+++ SurfReport/SurfReportApp.swift
@@ -6,10 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct SurfReportApp: App {
     @StateObject private var authStore = AuthenticationStore()
+
+    init() {
+        FirebaseApp.configure()
+    }
 
     var body: some Scene {
         WindowGroup {
